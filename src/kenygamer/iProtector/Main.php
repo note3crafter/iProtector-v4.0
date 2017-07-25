51e0e72f6efe2cfc4b9c946b506219f093a598c7
Kevin Andrews <kenygamer@users.noreply.github.com>
2017-07-24 21:34:02 -0400

    Implement API 3.0.0-ALPHA7 support

--- src/kenygamer/iProtector/Main.php
+++ src/kenygamer/iProtector/Main.php
@@ -130,7 +130,7 @@ class Main extends PluginBase implements Listener
      *
      * @return bool
      */
-    public function onCommand(CommandSender $p, Command $cmd, $label, array $args)
+    public function onCommand(CommandSender $p, Command $cmd, string $label, array $args)
     {
         if (!($p instanceof Player)) {
             $p->sendMessage(self::PREFIX . TextFormat::RED . "Command must be used in-game.");
